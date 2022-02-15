69ccaa86e28dbba1ab9101a37785e7b843fb18e2
Cam92 <9628366+Cam92@users.noreply.github.com>
2022-02-15 01:00:11 +0000

    Add files via upload

---
+++ scripts/Deploy.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+async function main() {
+
+  const Contract = await hre.ethers.getContractFactory("GweiGunslingers");
+  const contract = await Contract.deploy();
+
+  await contract.Deployed();
+
+  console.log("Gwei Gunslingers deployed to:", greeter.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/Duel.js
@@ -0,0 +1,24 @@
+const { ethers } = require("ethers");
+
+const GAME_ADDRESS = "0x4D95186c314f362eF74550ce970a1aAd949e1075";
+const URL = process.env.RINKEBY_URL;
+const provider = new ethers.providers.JsonRpcProvider(URL);
+
+async function main(shoot) {
+  const game = await hre.ethers.getContractAt("GweiGunslingers", GAME_ADDRESS);
+
+  const tx = await game.play(shoot);
+  const receipt = await tx.wait();
+
+  console.log(receipt);
+
+  console.log(await game.Entry());
+  console.log(await game.Duel());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
