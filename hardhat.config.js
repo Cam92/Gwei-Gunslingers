c0fe3053381d5828f830173c41c3b5994312bc3c
Cam92 <9628366+Cam92@users.noreply.github.com>
2022-03-17 23:46:14 +0000

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('@openzeppelin/hardhat-upgrades');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "ganache",
+  networks: {
+      ganache: {
+          url: "http://127.0.0.1:8545",
+          accounts: ["0xfd629d5879430fbb87ecc6bb0a2a0fde7a68e06a3d552a6ec728cf597e17e1a7"]
+      }
+  },
 };
